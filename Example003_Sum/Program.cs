b6ff57eefd0c8838f3c0eebfed920d05604ec7d4
ArtemDrozdov <drozdov_as@mail.ru>
2023-01-16 13:34:20 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿ int numberA = 3;
- int numberB = 5;
- int result = numberA + numberB;
+﻿int numberA = new Random().Next(0, 10); // случайное число от 0 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(0, 10); 
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
